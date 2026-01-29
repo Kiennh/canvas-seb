e22562faaa1273deb200ffb917647df7e5ae39e2
Kien <kien.nguyen@evgcorp.net>
2026-01-29 14:39:58 +0000

    ..

--- lib/canvas_seb/application_controller_extension.rb
+++ lib/canvas_seb/application_controller_extension.rb
@@ -10,21 +10,25 @@ module CanvasSeb
       # We only care if a user is logged in
       user = @current_user || (respond_to?(:current_user) && current_user)
       return unless user
-
+      
       settings = Canvas::Plugin.find(:canvas_seb).settings || {}
       return unless Canvas::Plugin.value_to_boolean(settings[:single_session])
 
       # Skip if this is the login/logout process to avoid recursion or blocking login
-      return if controller_name == 'pseudonym_sessions'
+      return if controller_name == 'pseudonym_sessions' || controller_name == 'login' || controller_path.to_s.start_with?('login/')
 
       current_token = session[:canvas_seb_session_token]
       
       cache_key = "user_single_session_#{user.id}"
-      authorized_token = Rails.cache.read(cache_key)
+      authorized_token = MultiCache.cache.read(cache_key)
 
-      # If there's an authorized token in cache but it doesn't match our session token, force logout
-      if authorized_token.present? && authorized_token != current_token
-        Rails.logger.warn "[Canvas SEB] Single Session - Token mismatch for user #{user.id}. Session Token: #{current_token || 'MISSING'}, Authorized Token: #{authorized_token}. Forcing logout."
+      Rails.logger.info "[Canvas SEB] Single Session - User: #{user.id}, Session Token: #{current_token.inspect}, Authorized Token: #{authorized_token.inspect}"
+
+      # If there's an authorized token in cache but it doesn't match our session token, force logout.
+      # We only force logout if the current session ALREADY has a token. 
+      # If current_token is blank, it's a new session that should be allowed to take over (invalidate the old one).
+      if current_token.present? && authorized_token.present? && authorized_token != current_token
+        Rails.logger.warn "[Canvas SEB] Single Session - Token mismatch for user #{user.id}. Session Token: #{current_token}, Authorized Token: #{authorized_token}. Forcing logout."
         
         # Force logout
         if respond_to?(:logout_current_user, true)
@@ -37,13 +41,14 @@ module CanvasSeb
         redirect_to login_url
       elsif authorized_token.blank? && current_token.present?
         # If for some reason the cache is empty but we have a token, re-populate the cache
-        Rails.cache.write(cache_key, current_token)
+        MultiCache.cache.write(cache_key, current_token)
       elsif current_token.blank?
-        # If the user is logged in but has no token (e.g. they were logged in before the plugin was enabled)
-        # We should probably assign them a token now to start enforcement
+        # If the user is logged in but has no token (e.g. new session or plugin just enabled)
+        # We assign them a new token and update the cache, which will invalidate any previous sessions.
         new_token = SecureRandom.uuid
         session[:canvas_seb_session_token] = new_token
-        Rails.cache.write(cache_key, new_token)
+        MultiCache.cache.write(cache_key, new_token)
+        Rails.logger.info "[Canvas SEB] Single Session - New session takeover for user #{user.id}. New Token: #{new_token}"
       end
     end
   end
